c32ffb3d30bcab10bfb4a4d5275b1c69e4dad2bc
mateuszgrzesiukiewicz <ajdija@gmail.com>
2018-09-22 23:27:13 +1000

    intersection in NavigateButton

--- src/Chapter_11_Type_checking_patterns/Example_3_union_type_intersection_type/src/common/NavigateButton/view.tsx
+++ src/Chapter_11_Type_checking_patterns/Example_3_union_type_intersection_type/src/common/NavigateButton/view.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import { Button } from 'react-native';
 import {
-    NavigationParams, NavigationScreenProp, NavigationState
+    NavigationInjectedProps,
+    NavigationParams,
 } from 'react-navigation';
 
 type NavigateButtonProps = {
     to: 'string',
     data: any,
-    text: 'string',
-    navigation: NavigationScreenProp<NavigationState, NavigationParams>
-};
+    text: 'string'
+} & NavigationInjectedProps<NavigationParams>;
 
 export const NavigateButton:React.SFC<NavigateButtonProps> = ({
     navigation, to, data, text
